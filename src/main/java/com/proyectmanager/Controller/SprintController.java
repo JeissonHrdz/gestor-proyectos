6ed4410a94930ead0ec700b2a10aa3725c8ab643
Jeisson Hernandez <jeissonhrdz186@gmail.com>
2024-06-14 10:54:30 -0500

    Configuracion envio sprint

--- src/main/java/com/proyectmanager/Controller/SprintController.java
+++ src/main/java/com/proyectmanager/Controller/SprintController.java
@@ -6,14 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.proyectmanager.Exceptions.ResourceNotFoundException;
 import com.proyectmanager.Model.Dto.SprintDto;
@@ -23,6 +16,7 @@ import com.proyectmanager.Services.ISprintService;
 
 @RestController
 @RequestMapping("/app/proyect/{idProyect}")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SprintController {
 
     @Autowired
